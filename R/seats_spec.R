e80f002060f769c9eae0f66da3cedf0e56f46c83
Smyk Anna <YWYD5I@ad.insee.intra>
2023-04-25 09:57:47 +0200

    set_seats doc minor changes

--- R/seats_spec.R
+++ R/seats_spec.R
@@ -2,23 +2,24 @@
 #' Set Seats Specification
 #'
 #' @description
-#' Function allowing to customize parameters in the Seats (decomposition) part of a Tramo-Seats.
-#' Seats is an Arima Model Based decomposition algorithm working in conjunction with Tramo.
+#' Function allowing to customize parameters in the decomposition part (Seats) of a Tramo-Seats
+#' seasonal adjustment process.
+#' (Seats is an Arima Model Based decomposition algorithm working in conjunction with Tramo.)
 #'
 #' @param x the specification to be modified, object of class `"JD3_TRAMOSEATS_SPEC"`, has to be generated with `spec_tramoseats()` function
 #' @param approximation character: the approximation mode. When the ARIMA model estimated by TRAMO does not accept an admissible decomposition, SEATS: `"None"` - performs an approximation; `"Legacy"` - replaces the model with a decomposable one; `"Noisy"` - estimates a new model by adding a white noise to the non-admissible model estimated by TRAMO. Default="Legacy".
 #' @param trend.boundary numeric: the trend boundary (rmod). The boundary beyond which an AR root is integrated in the trend component.
 #' If the modulus of the inverse real root is greater than the trend boundary, the AR root is integrated in the trend component.
-#' Below this value, the root is integrated in the transitory component. Possible values [0,1]. Default=0.5.
-#' @param seas.boundary numeric: the seasonal boundary (sbound). The boundary beyond which a real negative AR root is integrated in the seasonal component. If the modulus of the inverse negative real root is greater (or equal) than Seasonal boundary, the AR root is integrated into the seasonal component. Otherwise the root is integrated into the trend or transitory component. Possible values [0,1]. Default=0.8.
+#' Below this value, the root is integrated in the transitory component. Possible values \[0,1\]. Default=0.5.
+#' @param seas.boundary numeric: the seasonal boundary (sbound). The boundary beyond which a real negative AR root is integrated in the seasonal component. If the modulus of the inverse negative real root is greater (or equal) than Seasonal boundary, the AR root is integrated into the seasonal component. Otherwise the root is integrated into the trend or transitory component. Possible values \[0,1\]. Default=0.8.
 #' @param seas.boundary.unique numeric: the seasonal boundary (unique),(sboundatpi). The boundary beyond which a negative AR root is integrated
-#' in the seasonal component, when the root is the unique seasonal root.  If the modulus of the inverse negative real root is greater (or equal) than Seasonal boundary, the AR root is integrated into the seasonal component. Otherwise the root is integrated into the trend or transitory component. Possible values [0,1]. Default=0.8.
+#' in the seasonal component, when the root is the unique seasonal root.  If the modulus of the inverse negative real root is greater (or equal) than Seasonal boundary, the AR root is integrated into the seasonal component. Otherwise the root is integrated into the trend or transitory component. Possible values \[0,1\]. Default=0.8.
 #' @param seas.tolerance numeric: the seasonal tolerance (epsphi). The tolerance (measured in degrees) to allocate the AR non-real roots
 #' to the seasonal component (if the modulus of the inverse complex AR root is greater than the trend boundary
 #' and the frequency of this root differs from one of the seasonal frequencies by less than Seasonal tolerance)
-#' or the transitory component (otherwise). Possible values in [0,10]. Default value 2.
-#' @param ma.boundary numeric: the MA unit root boundary. When the modulus of an estimated MA root falls in the range (xl, 1),
-#' it is set to xl. Possible values [0.9,1]. Default=0.95.
+#' or the transitory component (otherwise). Possible values in \[0,10\]. Default value 2.
+#' @param ma.boundary numeric: the MA unit root boundary. When the modulus of an estimated MA root falls in the range \[xl, 1\],
+#' it is set to xl. Possible values \[0.9,1\]. Default=0.95.
 #' @param algorithm character: the estimation method for the unobserved components. The choice can be made from:
 #'
 #' - `"Burman"`: the default value. May result in a significant underestimation of the components' standard deviation,
